45497796777c4f5c2135cce595c55b58515414b3
CrumpetsNTea <t.botell@gmail.com>
2021-11-03 17:10:19 -0400

    working wordSearch function with vertical testing functionality. all tests passing. Coded with Nick Y.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const rows = letters.length;
+  const columns = letters[0].length;
+  const empty = [];
+  for (let i = 0; i < columns; i++) {
+    empty[i] = [];
+  }
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      empty[i].push(letters[j][i]);
     }
-}
+  }
 
-module.exports = wordSearch
+  const verticalJoin = empty.map(em => em.join(''));
+  for (let i of verticalJoin) {
+    if (i.includes(word)) {
+      return true;
+    }
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+    
+module.exports = wordSearch;
